3873098d25e2c8ac7147567f78458533896633fa
Nils Jeisecke <nils.jeisecke@saltation.com>
2020-02-11 17:39:27 +0100

    initial

---
+++ Program.fs
@@ -0,0 +1,63 @@
+﻿open System
+
+open FSharp.Data.Sql
+
+FSharp.Data.Sql.Common.QueryEvents.SqlQueryEvent |> Event.add (printfn "Executing SQL: %O")
+
+[<Literal>]
+let ConnString  = "Server=localhost;Database=test;User=njeiseck;Password=password"
+
+[<Literal>]
+let DbVendor    = Common.DatabaseProviderTypes.MYSQL
+
+[<Literal>]
+let ContextSchemaPath =  __SOURCE_DIRECTORY__ + @"/test.schema"
+
+type Sql = SqlDataProvider<DatabaseVendor = DbVendor,
+                           ConnectionString = ConnString,
+                           ContextSchemaPath = ContextSchemaPath,
+                           Owner = "Test",
+                           UseOptionTypes = true
+                           >
+
+let ctx = Sql.GetDataContext()
+
+let query1 = ctx.Test.People 
+          |> Seq.map (fun e -> e.ColumnValues |> Seq.toList)
+          |> Seq.toList
+
+let query2 =
+    query {
+      for t in ctx.Test.People do
+        where (t.FirstName.Value.Contains "F") // Name is Nullable
+        select (t.FirstName, t.LastName, t.Info, t.Id)
+    } |> Seq.toList
+
+[<EntryPoint>]
+let main argv =
+
+    // Query some data
+    printfn "Q1: %A" query1
+    printfn "Q2: %A" query2
+
+    // Add
+    let rows = 
+      seq { 
+        for i in 1 .. 10 do
+          let row = ctx.Test.People.Create()
+          row.FirstName <- Some "First Name";
+          row.LastName <- Some "Last Name"
+          yield row
+      } |> Seq.toList
+
+    ctx.SubmitUpdates()
+
+    // print generated ids
+    for row in rows do
+      printfn "New Id %A" row.Id
+
+    // Rather crude way to store the schema data so no actual DB is needed in CI
+    //printfn "Context Schema: %s" ContextSchemaPath
+    //ctx.SaveContextSchema() |> ignore
+
+    0 // exit code
---
+++ people.sql
@@ -0,0 +1,53 @@
+-- MariaDB dump 10.17  Distrib 10.4.11-MariaDB, for osx10.14 (x86_64)
+--
+-- Host: localhost    Database: Test
+-- ------------------------------------------------------
+-- Server version	10.4.11-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `People`
+--
+
+DROP TABLE IF EXISTS `People`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `People` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `info` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `People`
+--
+
+LOCK TABLES `People` WRITE;
+/*!40000 ALTER TABLE `People` DISABLE KEYS */;
+INSERT INTO `People` VALUES (76,'Flintstone','Fred',NULL),(77,'Foo','Bar','Nerd'),(78,'First Name','Last Name',NULL),(79,'First Name','Last Name',NULL),(80,'First Name','Last Name',NULL),(81,'First Name','Last Name',NULL),(82,'First Name','Last Name',NULL),(83,'First Name','Last Name',NULL),(84,'First Name','Last Name',NULL),(85,'First Name','Last Name',NULL),(86,'First Name','Last Name',NULL),(87,'First Name','Last Name',NULL);
+/*!40000 ALTER TABLE `People` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-02-11 17:29:37
